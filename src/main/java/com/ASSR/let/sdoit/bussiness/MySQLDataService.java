a143d842fa0c54ca9db113bf538334fa7298fd33
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2022-12-28 23:35:02 +0530

    Array changed

--- src/main/java/com/ASSR/let/sdoit/bussiness/MySQLDataService.java
+++ src/main/java/com/ASSR/let/sdoit/bussiness/MySQLDataService.java
@@ -10,7 +10,7 @@ public class MySQLDataService implements DataService{
 	@Override
 	public int[] retriveData() {
 		// TODO Auto-generated method stub
-		return new int[] {5,7,6,8,45,34,65,78};
+		return new int[] {5,7,6,10,45,34,65,78};
 	}
 
 }
